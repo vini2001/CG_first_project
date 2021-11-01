b4d835441126f0d7e4719d8c71323c428f45b5b2
Vinícius Neves <vassisn2001@gmail.com>
2021-11-01 20:30:30 -0300

    Initial Commit

---
+++ LearnOpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LearnOpenGL
+//
+//  Created by Vinicius Neves on 01/11/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
